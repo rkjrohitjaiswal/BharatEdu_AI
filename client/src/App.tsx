0e2b63ac0f0bc6297c29840139163ad446b39f96
Rohit Kumar Jaiswal <70046863+rkjrohitjaiswal@users.noreply.github.com>
2026-08-20 01:15:31 +0530

    feat: add career roadmap route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 import { LandingPage } from './pages/LandingPage';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
-
 import { DashboardPage } from './pages/DashboardPage';
 import { LearningCoachPage } from './pages/LearningCoachPage';
 import { TutorPage } from './pages/TutorPage';
@@ -23,216 +22,51 @@ import { StudentGoalsPage } from './pages/StudentGoalsPage';
 import { AchievementsPage } from './pages/AchievementsPage';
 import { ExamPreparationPage } from './pages/ExamPreparationPage';
 import { ExamReadinessPage } from './pages/ExamReadinessPage';
-
+import { CareerRoadmapPage } from './pages/CareerRoadmapPage';
 import { TeacherDashboardPage } from './pages/TeacherDashboardPage';
 import { TeacherStudentsPage } from './pages/TeacherStudentsPage';
 import { TeacherAnalyticsPage } from './pages/TeacherAnalyticsPage';
 import { TeacherInterventionsPage } from './pages/TeacherInterventionsPage';
-
 import { ParentDashboardPage } from './pages/ParentDashboardPage';
 import { ParentStudentOverviewPage } from './pages/ParentStudentOverviewPage';
 import { ParentLinkPage } from './pages/ParentLinkPage';
 
-export const App: React.FC = () => {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            {/* Public Routes */}
-            <Route index element={<LandingPage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-
-            {/* Student Protected Routes */}
-            <Route
-              path="dashboard"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <DashboardPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="learning-coach"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <LearningCoachPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="tutor"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <TutorPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="practice"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <PracticePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="learning-path"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <LearningPathPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="progress"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <ProgressPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="scholarships"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <ScholarshipsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="scholarships/saved"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <SavedScholarshipsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="mistakes"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <MistakeReviewPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="practice-history"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <PracticeHistoryPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="interventions"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <StudentInterventionsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="goals"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <StudentGoalsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="achievements"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <AchievementsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="exam-prep"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <ExamPreparationPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="exam-prep/:id/readiness"
-              element={
-                <ProtectedRoute allowedRoles={['student']}>
-                  <ExamReadinessPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Teacher Protected Routes */}
-            <Route
-              path="teacher"
-              element={
-                <ProtectedRoute allowedRoles={['teacher']}>
-                  <TeacherDashboardPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="teacher/interventions"
-              element={
-                <ProtectedRoute allowedRoles={['teacher']}>
-                  <TeacherInterventionsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="teacher/students"
-              element={
-                <ProtectedRoute allowedRoles={['teacher']}>
-                  <TeacherStudentsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="teacher/analytics"
-              element={
-                <ProtectedRoute allowedRoles={['teacher']}>
-                  <TeacherAnalyticsPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Parent Protected Routes */}
-            <Route
-              path="parent/dashboard"
-              element={
-                <ProtectedRoute allowedRoles={['parent']}>
-                  <ParentDashboardPage user={null} />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="parent/overview/:studentId"
-              element={
-                <ProtectedRoute allowedRoles={['parent']}>
-                  <ParentStudentOverviewPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="parent-link"
-              element={
-                <ProtectedRoute allowedRoles={['student', 'parent']}>
-                  <ParentLinkPage user={null} />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Catch-all fallback */}
-            <Route path="*" element={<LandingPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
-};
+export const App: React.FC = () => (
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="dashboard" element={<ProtectedRoute allowedRoles={['student']}><DashboardPage /></ProtectedRoute>} />
+          <Route path="learning-coach" element={<ProtectedRoute allowedRoles={['student']}><LearningCoachPage /></ProtectedRoute>} />
+          <Route path="career" element={<ProtectedRoute allowedRoles={['student']}><CareerRoadmapPage /></ProtectedRoute>} />
+          <Route path="tutor" element={<ProtectedRoute allowedRoles={['student']}><TutorPage /></ProtectedRoute>} />
+          <Route path="practice" element={<ProtectedRoute allowedRoles={['student']}><PracticePage /></ProtectedRoute>} />
+          <Route path="learning-path" element={<ProtectedRoute allowedRoles={['student']}><LearningPathPage /></ProtectedRoute>} />
+          <Route path="progress" element={<ProtectedRoute allowedRoles={['student']}><ProgressPage /></ProtectedRoute>} />
+          <Route path="scholarships" element={<ProtectedRoute allowedRoles={['student']}><ScholarshipsPage /></ProtectedRoute>} />
+          <Route path="scholarships/saved" element={<ProtectedRoute allowedRoles={['student']}><SavedScholarshipsPage /></ProtectedRoute>} />
+          <Route path="mistakes" element={<ProtectedRoute allowedRoles={['student']}><MistakeReviewPage /></ProtectedRoute>} />
+          <Route path="practice-history" element={<ProtectedRoute allowedRoles={['student']}><PracticeHistoryPage /></ProtectedRoute>} />
+          <Route path="interventions" element={<ProtectedRoute allowedRoles={['student']}><StudentInterventionsPage /></ProtectedRoute>} />
+          <Route path="goals" element={<ProtectedRoute allowedRoles={['student']}><StudentGoalsPage /></ProtectedRoute>} />
+          <Route path="achievements" element={<ProtectedRoute allowedRoles={['student']}><AchievementsPage /></ProtectedRoute>} />
+          <Route path="exam-prep" element={<ProtectedRoute allowedRoles={['student']}><ExamPreparationPage /></ProtectedRoute>} />
+          <Route path="exam-prep/:id/readiness" element={<ProtectedRoute allowedRoles={['student']}><ExamReadinessPage /></ProtectedRoute>} />
+          <Route path="teacher" element={<ProtectedRoute allowedRoles={['teacher']}><TeacherDashboardPage /></ProtectedRoute>} />
+          <Route path="teacher/interventions" element={<ProtectedRoute allowedRoles={['teacher']}><TeacherInterventionsPage /></ProtectedRoute>} />
+          <Route path="teacher/students" element={<ProtectedRoute allowedRoles={['teacher']}><TeacherStudentsPage /></ProtectedRoute>} />
+          <Route path="teacher/analytics" element={<ProtectedRoute allowedRoles={['teacher']}><TeacherAnalyticsPage /></ProtectedRoute>} />
+          <Route path="parent/dashboard" element={<ProtectedRoute allowedRoles={['parent']}><ParentDashboardPage user={null} /></ProtectedRoute>} />
+          <Route path="parent/overview/:studentId" element={<ProtectedRoute allowedRoles={['parent']}><ParentStudentOverviewPage /></ProtectedRoute>} />
+          <Route path="parent-link" element={<ProtectedRoute allowedRoles={['student', 'parent']}><ParentLinkPage user={null} /></ProtectedRoute>} />
+          <Route path="*" element={<LandingPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  </AuthProvider>
+);
 
 export default App;
